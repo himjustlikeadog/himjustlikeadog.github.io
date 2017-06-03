dd7ecf53d11c69dc76959071cd302df2fc2d73d0
himjustlikeadog <2444570699@qq.com>
2017-06-03 18:03:44 +0800

    Site updated: 2017-06-03 18:03:44

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('this blog||of|黄强|#rectangle|#countdown 3||');
+      S.UI.simulate('this blog|of|黄强|Welcome here|#rectangle|#countdown 3||');
     }
 
     S.Drawing.loop(function () {
